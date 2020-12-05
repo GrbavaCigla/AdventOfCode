3c5ba9d4b877a1a4ab574d8d2282503c1f6855dd
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2020-12-05 18:16:45 +0100

    5th day

---
+++ 5/1.py
@@ -0,0 +1,31 @@
+with open("input", "r") as file:
+    text = file.read().splitlines()
+
+def decode_seat(seat: str):
+    lower = 0
+    upper = 127
+    left = 0
+    right = 7
+
+    ans1, ans2 = 0, 0
+
+    for i, letter in enumerate(seat):
+        if letter == 'F':
+            upper = lower + (upper - lower)//2
+            ans1 = upper
+        elif letter == 'B':
+            lower = lower + (upper - lower)//2 + 1
+            ans1 = lower
+        
+        elif letter == 'L':
+            right = left + (right - left)//2
+            ans2 = right
+        elif letter == 'R':
+            left = left + (right - left)//2 + 1
+            ans2 = left
+        
+    return ans1*8+ans2
+
+ids = [decode_seat(i) for i in text]
+print(ids)
+print(max(ids))
