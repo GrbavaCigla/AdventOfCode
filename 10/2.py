11efe5e5b462179605facc042ce6c4fea7833035
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2020-12-11 22:40:18 +0100

    11 day done

--- 10/2.py
+++ 10/2.py
@@ -1,18 +1,16 @@
-from pprint import pprint
-
 with open("input", "r") as file:
     text = file.read().splitlines()
     text = sorted(map(int, text))
 
 
-sol = {0:1}
-for line in sorted(text):
-    sol[line] = 0
-    if line - 1 in sol:
-        sol[line]+=sol[line-1]
-    if line - 2 in sol:
-        sol[line]+=sol[line-2]
-    if line - 3 in sol:
-        sol[line]+=sol[line-3]
+seqs = {0:1}
+for line in text:
+    seqs[line] = 0
+    if line - 1 in seqs:
+        seqs[line] += seqs[line-1]
+    if line - 2 in seqs:
+        seqs[line] += seqs[line-2]
+    if line - 3 in seqs:
+        seqs[line] += seqs[line-3]
 
-print(sol[max(text)])
+print(seqs[max(text)])
