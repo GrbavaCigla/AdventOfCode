3a69bed3ba82bb1305387017f5252d915d0cb806
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2021-12-15 23:16:04 +0100

    Second part of 14th day done

--- 2021/14.rs
+++ 2021/14.rs
@@ -1,50 +1,51 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
-fn next_step(input: &Vec<char>, pairs: &HashMap<&str, char>) -> Vec<char> {
-    let mut ans = vec![];
+fn solve(depth: usize, template: &Vec<char>, pairs: &HashMap<(char, char), char>) -> u64 {
+    let mut pair_count = HashMap::<(char, char), u64>::new();
 
-    for i in input.windows(2) {
-        let pair = format!("{}{}", i[0], i[1]);
-        let slice = &pair[..];
+    for i in template.windows(2) {
+        *pair_count.entry((i[0], i[1])).or_insert(0) += 1;
+    }
 
-        ans.push(i[0]);
-        if pairs.contains_key(&slice) {
-            ans.push(*pairs.get(slice).unwrap());
+    for _ in 0..depth {
+        for (pair, count) in pair_count.clone() {
+            if let Some(x) = pairs.get(&pair) {
+                *pair_count.entry((pair.0, *x)).or_insert(0) += count;
+                *pair_count.entry((*x, pair.1)).or_insert(0) += count;
+                *pair_count.get_mut(&pair).unwrap() -= count;
+            }
         }
     }
-    ans.push(*input.last().unwrap());
 
-    ans
+    let mut counts = HashMap::<char, u64>::new();
+    for i in pair_count.iter() {
+        *counts.entry(i.0.0).or_insert(0) += i.1;
+        *counts.entry(i.0.1).or_insert(0) += i.1;
+    }
+
+    *counts.entry(*template.first().unwrap()).or_insert(0) += 1;
+    *counts.entry(*template.last().unwrap()).or_insert(0) += 1;
+
+    (counts.values().max().unwrap() - counts.values().min().unwrap()) / 2
 }
 
 fn main() {
     let mut input = include_str!("inputs/14").lines();
 
-    let mut template: Vec<char> = input.next().unwrap().chars().collect();
+    let template: Vec<char> = input.next().unwrap().chars().collect();
 
     input.next();
 
-    let pairs: HashMap<&str, char> = input
+    let pairs: HashMap<(char, char), char> = input
         .map(|x| x.split_once(" -> ").unwrap())
-        .map(|x| (x.0, x.1.chars().next().unwrap()))
+        .map(|x| {
+            (
+                (x.0.chars().nth(0).unwrap(), x.0.chars().nth(1).unwrap()),
+                x.1.chars().next().unwrap(),
+            )
+        })
         .collect();
 
-    for i in 0..5 {
-        template = next_step(&template, &pairs);
-    }
-
-    println!("{:?}", template);
-
-    let chars: HashSet<char> = template.clone().into_iter().collect();
-    let chars: Vec<char> = chars.into_iter().collect();
-
-    let occurances = template.iter().fold(vec![0; chars.len()], |acc, x| {
-        let mut new_acc = acc.clone();
-
-        new_acc[chars.iter().position(|y| y == x).unwrap()] += 1;
-
-        new_acc
-    });
-
-    println!("First part result: {}", occurances.iter().max().unwrap() - occurances.iter().min().unwrap());
+    println!("First part result: {}", solve(10, &template, &pairs));
+    println!("Second part result: {}", solve(40, &template, &pairs));
 }
